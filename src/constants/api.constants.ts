e53264fdc4afa1454ef936bc521578be11f2156a
Matthieu Chollet <matthieuchollet@MacBook-Pro-de-Matthieu.local>
2023-06-14 15:47:03 +0200

    chore: update token

--- src/constants/api.constants.ts
+++ src/constants/api.constants.ts
@@ -1,5 +1,5 @@
 export const BASE_API = 'https://api.spotify.com/v1'
 export const TOKEN =
-  'BQA0XEdoWfcC58SJUe5p7wWGmJ9lp5dLY4Dk6efZFmCUKmWiXA2QhSGHn6wEeFc9bmCu1kRG_Hs1NdIrmScdq0RCDnqPWeOE9YUnarmpRMU-W5iAIf7fUt3C0kJuWoPoe6eiTVso6RyjPo6fJGuU5YfqaDgXJGgZs4W8mAkK0hgEE8LphmjluQLcrmaVfCqyZPhItG40S4z0XV6lheIPpmQUib8'
+  'BQCR44K6-8i3jz4mjJIHhWaw9ga6u8wQcrCDoTBOrQQCuDpJOsswx3I7fVS0_rhSJcsRGKUbrOQPyVxEC3URvloX875AOE2WSbgyo4sp1wusQu0Dv20GILPCxvNiZr0QmVbSdzQ-j-u_b5tVYNxqIrjOPK2fjD8fodsmK5Y7iqi0nBWFh5gzWA6HMX9DIrfW_j2oWm-FXOIevevcCBGYVlGI77U'
 export const DEFAULT_OFFSET = 0
 export const DEFAULT_LIMIT = 50
